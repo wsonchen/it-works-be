2aabad87f3e675cef6adbcf4875646738b0f0f95
Wilson <wilson_chen@bca.co.id>
2021-08-22 11:18:50 +0000

    00-init commit

---
+++ src/main/java/coid/bca/gsit/GsitApplication.java
@@ -0,0 +1,13 @@
+package coid.bca.gsit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coid/bca/gsit/GsitApplicationTests.java
@@ -0,0 +1,13 @@
+package coid.bca.gsit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
