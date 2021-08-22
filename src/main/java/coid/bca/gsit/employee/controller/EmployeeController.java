30e3fd588861a230ddfcbd0bbb4314c08d138fca
Wilson <wilson_chen@bca.co.id>
2021-08-22 14:47:54 +0000

    allow origin

--- src/main/java/coid/bca/gsit/employee/controller/EmployeeController.java
+++ src/main/java/coid/bca/gsit/employee/controller/EmployeeController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
