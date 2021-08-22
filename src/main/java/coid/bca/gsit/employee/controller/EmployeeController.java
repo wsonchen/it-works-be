1ca0ff13fdaaab8ad9df7baba9ff7f4d50889e19
Wilson <wilson_chen@bca.co.id>
2021-08-22 13:17:43 +0000

    read feature

--- src/main/java/coid/bca/gsit/employee/controller/EmployeeController.java
+++ src/main/java/coid/bca/gsit/employee/controller/EmployeeController.java
@@ -1,17 +1,30 @@
 package coid.bca.gsit.employee.controller;
 
+import coid.bca.gsit.employee.model.Employee;
+import coid.bca.gsit.employee.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeService service;
+
     @GetMapping
     public ResponseEntity<?> getAllEmployees() {
-        return ResponseEntity.status(HttpStatus.OK).body("Hello World!");
+        List<Employee> employees = service.getAllEmployees();
+
+        if (employees.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(employees);
     }
 }
